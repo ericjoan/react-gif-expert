c55aac0c94d240035d5ad6a19edc20268ad01fec
eric <ericjoan22@gmail.com>
2023-06-09 19:32:29 -0500

    getGifs actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) =>{
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=iaChLeXMLK83oD859ZUdAsMiWWnrsbKR&q=${ category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=iaChLeXMLK83oD859ZUdAsMiWWnrsbKR&q=${ category}&limit=20`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
